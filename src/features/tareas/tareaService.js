57148604878f93ded7642bb113b76e92316c5b29
Fabricio Suarez <codificando.mx@gmail.com>
2025-11-08 13:56:22 -0600

    Crear tareas ok

---
+++ src/features/tareas/tareaService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_URL = 'https://backendnodea-5ufv.onrender.com/api/tareas/'
+
+//Crear Tarea
+const crearTarea = async (tareaData, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.post(API_URL, tareaData, config)
+
+    return response.data
+}
+
+const tareaService = {
+    crearTarea
+}
+
+export default tareaService
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import TareaForm from '../components/TareaForm'
+import Spinner from '../components/Spinner'
 
 const Dashboard = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user} = useSelector((state)=> state.auth)
+
   return (
-    <div>Dashboard</div>
+    <>
+      <section className="heading">
+        <h3>Bienvenido {user && user.nombre}</h3>
+        <p>Dashboard de Tareas App</p>
+      </section>
+      <TareaForm />
+    </>
+    
   )
 }
 
