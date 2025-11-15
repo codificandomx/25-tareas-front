bb2cd9c6037ed3dd53911c8847315eeddf55f6e1
Fabricio Suarez <codificando.mx@gmail.com>
2025-11-15 12:26:46 -0600

    Version Final

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import TareaForm from '../components/TareaForm'
 import Spinner from '../components/Spinner'
+import {getTareas, reset} from '../features/tareas/tareaSlice'
+import { toast } from 'react-toastify'
+import TareaItem from '../components/TareaItem'
 
 const Dashboard = () => {
 
@@ -10,6 +13,25 @@ const Dashboard = () => {
   const dispatch = useDispatch()
 
   const {user} = useSelector((state)=> state.auth)
+  const { tareas, isLoading, isError, isSuccess, message } = useSelector((state)=> state.tarea)
+
+  useEffect(()=>{
+
+    if (isError) {
+      toast.error(message)
+    }
+
+    if(!user) {
+      navigate('/login')
+    } else {
+      dispatch(getTareas())
+    }
+
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -17,9 +39,13 @@ const Dashboard = () => {
         <h3>Bienvenido {user && user.nombre}</h3>
         <p>Dashboard de Tareas App</p>
       </section>
+
       <TareaForm />
+
+      <section className="content">
+       {console.log(tareas)}
+      </section>
     </>
-    
   )
 }
 
